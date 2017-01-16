324cfd6e34a75675e7b97b6ee8d535281930c0b0
Alex Li <phantomgale@hotmail.com>
2017-01-16 21:24:28 +0800

    Moogose&api

--- app.js
+++ app.js
@@ -1,46 +1,54 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+/**
+ * Created by Li on 2017/1/16.
+ * Code to run Mongod on Windows
+ * open cmd first, and copy those code
+ *
+ *================================================
+ * cd C:\Program Files\MongoDB\Server\3.2\bin
+ * mongod --dbpath C:\Projects\data\db
+ *================================================
+ *
+ * On Linux ubuntu start code is
+ *
+ * ===============================================
+ *sudo service mongod start
+ *================================================
+ *
+ *
+ *stop code is
+ *================================================
+ *sudo service mongod stop
+ *================================================
+ *
+ *
+ *
+ * Modules:
+ * mongoose@4.7.7
+ *
+ *
+ *
+ */
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var express = require('express');
+var bodyParser = require('body-parser');
+var api = require('./routes/api');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+var db = mongoose.connection;
+
+db.on('error', function (err) {
+    console.log('Mongo connection error. Please check Mongo is running on the host');
+    console.log(err);
+});
+db.once('open', function () {
+    console.log('MongoDb connected.');
+});
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.listen(3000);
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.use(bodyParser.urlencoded({extended:false}));
+app.use('/api',api);
---
+++ routes/api.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Li on 2017/1/16.
+ */
+var express = require('express');
+var router = express.Router();
+
+
+
+
+
+module.exports = router;
