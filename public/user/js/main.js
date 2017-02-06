0b76bc9415d476401e2e7c6d968eb564454ff421
denn99 <davidchd@outlook.com>
2017-02-06 18:34:27 +0800

    WatchCat

--- public/user/js/main.js
+++ public/user/js/main.js
@@ -2,3 +2,27 @@
  * Created by David on 06/02/2017.
  */
 var app = angular.module('User', ['ui.router', 'ngMessages']);
+app.config(function($stateProvider, $urlRouterProvider) {
+    var login = {
+        name: 'login',
+        url: '/login',
+        controller: 'LoginCont',
+        templateUrl: 'html/login.html'
+    };
+    var signup = {
+        name: 'signup',
+        url: '/signup',
+        controller: 'SignCont',
+        templateUrl: 'html/signUp.html'
+    };
+    var retrieve = {
+        name: 'retrieve',
+        url: '/retrieve',
+        controller: 'RetrCont',
+        templateUrl: 'html/retrieve.html'
+    };
+    $stateProvider.state(login);
+    $stateProvider.state(signup);
+    $stateProvider.state(retrieve);
+    $urlRouterProvider.otherwise('/login');
+});
