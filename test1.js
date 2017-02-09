45e53827887776d3b3f1728b5ae6821a628a0f00
Alex Li <phantomgale@hotmail.com>
2017-02-09 18:59:59 +0800

    testing

---
+++ test1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by phant on 2017/2/9.
+ */
+var test2 = require('test2');
+var ans = 1;
+console.log(test2.f1(ans));
---
+++ test2.js
@@ -0,0 +1,7 @@
+/**
+ * Created by phant on 2017/2/9.
+ */
+
+var f1 = function(var1){
+    return var1++;
+};
