bd79eb6ddccb21e0894882e5de5e0eb15ea240d6
denn99 <davidchd@outlook.com>
2017-02-15 14:01:26 +0800

    WatchCat
    前端的Directive已完成

--- public/user/js/directive.js
+++ public/user/js/directive.js
@@ -1,3 +1,77 @@
 /**
  * Created by David on 06/02/2017.
  */
+angular.module('User').directive('email', function() {
+    var filter = /^[A-Za-zd]+([-_.][A-Za-zd]+)*@([A-Za-zd]+[-.])+[A-Za-zd]{2,5}$/;
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function(scope, elem, attr, ctrl) {
+            ctrl.$validators.email = function(value) {
+                if(filter.test(value)) {
+                    return true;
+                }
+                return false;
+            };
+        }
+    }
+});
+angular.module('User').directive('first', function() {
+    var filter = /^[a-zA-Z]{1,10}$/;
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function(scope, elem, attr, ctrl) {
+            ctrl.$validators.first = function(value) {
+                if(filter.test(value)) {
+                    return true;
+                }
+                return false;
+            };
+        }
+    }
+});
+angular.module('User').directive('last', function() {
+    var filter = /^[a-zA-Z]{2,10}$/;
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function(scope, elem, attr, ctrl) {
+            ctrl.$validators.last = function(value) {
+                if(filter.test(value)) {
+                    return true;
+                }
+                return false;
+            };
+        }
+    }
+});
+angular.module('User').directive('pass', function() {
+    var filter = /^[\@A-Za-z0-9\!\#\$\%\^\&\*\.\~]{6,22}$/;
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function(scope, elem, attr, ctrl) {
+            ctrl.$validators.pass = function(value) {
+                if(filter.test(value)) {
+                    return true;
+                }
+                return false;
+            };
+        }
+    }
+});
+angular.module('User').directive('conf', function() {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function(scope, elem, attr, ctrl) {
+            ctrl.$validators.pass = function(value) {
+                if(value == scope.data.pass) {
+                    return true;
+                }
+                return false;
+            };
+        }
+    }
+});
