191295363daf79022e11958ef0a410f273c54136
denn99 <davidchd@outlook.com>
2017-02-18 19:47:46 +0800

    WatchCat
    前端JS已完成

--- public/user/js/controller/signup.js
+++ public/user/js/controller/signup.js
@@ -1,3 +1,19 @@
 /**
  * Created by David on 06/02/2017.
  */
+var app = angular.module('User');
+app.controller('LoginCont', function($scope, $state, $http) {
+    $scope.data = {};
+    $scope.goState = function(add) {
+        $state.go(add);
+    };
+    $scope.login = function() {
+        $http.post('/api', $scope.data).success(function(res){
+            if(res.status == "Success") {
+                window.location.href = "../index.html";
+            } else {
+                alert("Sorry, sign up failed.");
+            }
+        });
+    };
+});
