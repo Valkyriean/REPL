990589bc044e95a96376edadeb21876289baf404
Alex Li <phantomgale@hotmail.com>
2017-02-27 10:32:12 +0800

    C9 ready、

--- controllers/saveAccount.js
+++ controllers/saveAccount.js
@@ -2,3 +2,23 @@
  * Created by phant on 2017/2/27.
  */
 var User = require('../models/UserModel');
+
+
+
+exports.saveAccount = function(req,res,next){
+    var newUser = new User({
+        emailaddress: req.body.email,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: req.encrypted
+    });
+    newUser.save(function(err) {
+        if (err) {
+            res.json({"status": "savefailed"});
+        }else{
+            console.log('User saved successfully!');
+            res.json({"status": "success"});
+        }
+
+    });
+};
