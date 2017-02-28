e6dbbbbeb57b8bf30a24a6fff92139cb8e801a96
denn99 <davidchd@outlook.com>
2017-02-28 09:51:49 +0800

    WatchCat

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/**
+ * Created by David on 27/02/2017.
+ */
+var gulp=require("gulp");
+var nodemon=require("gulp-nodemon");
+
+
+gulp.task("default",[]);
+
+
+gulp.task("server",function () {
+    nodemon({
+        script:'app.js',
+        env:{'NODE_ENV':'development'}
+    });
+});
+
