3d551267cfe050b971c5e00f386b16baa3979980
Alex Li <phantomgale@hotmail.com>
2017-03-07 10:20:51 +0800

    test

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,5 @@ router.post('/token',readToken);
 router.post('/changePass',validation.passValidation,encryption.encryptBothPass,account.updateAccount);
 router.post('/deleteUser',encryption.encryptPass,account.deleteAccount);
 
+
 module.exports = router;
