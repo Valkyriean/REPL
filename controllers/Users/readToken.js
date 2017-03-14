d8f6ecdf6a8feea8af0a582bc551ccbded77f6c6
Alex Li <phantomgale@hotmail.com>
2017-03-14 10:10:20 +0800

    666

--- controllers/Users/readToken.js
+++ controllers/Users/readToken.js
@@ -7,11 +7,11 @@ exports.readToken = function(req,res,next){
     if(req.body.token == null){
         res.json({"status":"failed","message":"null token"});
     }else{
-        jwt.verify(req.body.token, secretKey,function(err,decodede){
+        jwt.verify(req.body.token, secretKey,function(err,decoded){
             if(err){
                 res.json({"status":"failed","message":"wrong or expired token"});
             }else{
-                User.findOne({'userID':decodede.data}, function(err,user){
+                User.findOne({'userID':decoded.data}, function(err,user){
                     if(err){
                         res.json({"status": "failed","message":"token not found"});
                         console.log("wrong token/n"+user.userID+"/n"+decoded)
