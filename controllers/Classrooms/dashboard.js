d688c8fb8cb025a54749d20f218dde38389b464a
Alex Li <phantomgale@hotmail.com>
2017-03-21 11:26:39 +0800

    nope

--- controllers/Classrooms/dashboard.js
+++ controllers/Classrooms/dashboard.js
@@ -2,17 +2,16 @@ var Classrooms = require('../../models/ClassroomsModel');
 var jwt = require('jsonwebtoken');
 var secretKey = require('../../Strings').secretKey;
 
-exports.postDashboard = function(req,res,next){
+exports.postDashboard = function(req,res){
     var data={
         status: 20,
-        token:jwt.sign({data: req.user.userID},secretKey, { expiresIn: '24h' }),
         type:req.user.type,
         teacher:null,
         own:null,
         student:null
     };
 
-    if(req.user.type == "teacher"){
+    if(req.user.type === "teacher"){
         Classrooms.findOne({'own': req.user.userID}, function(err, classroom) {
             if(err) throw err;
             data.own = classroom;
