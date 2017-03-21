df81635030295b3e8e544e9461911a1933b6d7e5
JoshMYan <yanboxiao1999@126.com>
2017-03-21 14:42:13 +0800

    hhh

--- public/users/controller/retrieve.js
+++ public/users/controller/retrieve.js
@@ -13,7 +13,7 @@ app.controller('RetrCont', function($scope, $state, $http) {
         $http.post('/api/users/lostPass', $scope.data).then(function(res){
             if(res.data.status == "success") {
                 alert("The new password has been sent to your email.");
-            } else if(res.status == "null") {
+            } else if(res.data.status == "null") {
                 alert("The email has not been registered yet.")
             } else {
                 alert("Sorry, retrieve failed.");
