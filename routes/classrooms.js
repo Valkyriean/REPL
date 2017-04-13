bbd93d0072bbc44bf0ee804d07bef8e21891d2fb
Alex Li <phantomgale@hotmail.com>
2017-04-13 05:21:32 +0000

    cls

--- routes/classrooms.js
+++ routes/classrooms.js
@@ -1,3 +1,13 @@
 /**
  * Created by Li on 2017/3/19.
  */
+var express = require('express');
+var router = express.Router();
+var dashBoard = require('../controllers/Classrooms/dashBoard');
+var newClassroom = require('../controllers/Classrooms/newClassroom');
+
+var decodeToken = require('../controllers/Users/decodeToken').readToken;
+
+//Dash Board
+router.post('/dashBoard',decodeToken,dashBoard.postDashboard);
+router.post('/newClassroom',decodeToken,newClassroom.newClassroom);
