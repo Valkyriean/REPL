4bde7a206a0dcbefa1d5c5d48dff66289d670933
Alex Li <phantomgale@hotmail.com>
2017-04-14 00:51:42 +0000

    2

--- routes/classrooms.js
+++ routes/classrooms.js
@@ -5,9 +5,11 @@ var express = require('express');
 var router = express.Router();
 var dashBoard = require('../controllers/Classrooms/dashBoard');
 var newClassroom = require('../controllers/Classrooms/newClassroom');
+var classroomSetting = require('../controllers/Classrooms/classroomSetting');
 
 var decodeToken = require('../controllers/Users/decodeToken').readToken;
 
 //Dash Board
 router.post('/dashBoard',decodeToken,dashBoard.postDashboard);
 router.post('/newClassroom',decodeToken,newClassroom.newClassroom);
+router.post('/classroomSetting',decodeToken,classroomSetting.updateClassroomSetting);
