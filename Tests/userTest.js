bc841f7df6464ba953a462b1c717590c67788b73
Alex Li <phantomgale@hotmail.com>
2017-05-25 11:57:31 +0800

    usertest 2.0

--- Tests/userTest.js
+++ Tests/userTest.js
@@ -7,6 +7,7 @@ chai.use(chaiHttp);
 let endPoint = "http://localhost:3000/api";
 
 describe('Users',() =>{
+	//sign up
 	it('it should sign up goodly (correct info)', (done) => {
 		chai.request(endPoint)
 			.post('/users/signup')
@@ -36,4 +37,74 @@ describe('Users',() =>{
 				done();
 			});
 	});
+
+	//passlogin
+	let token;
+	let loginToken;
+
+	it('it should use password log in goodly (correct info)', (done) => {
+		chai.request(endPoint)
+			.post('/users/passLogin')
+			.send({"email":"test1@email.com","pass":"testpass1"})
+			.end((err, res) => {
+				loginToken = res.body.loginToken;
+				token = res.body.token;
+				res.body.should.have.status(1);
+				done();
+			});
+	});
+
+	it('it shouldnt use password log in (wrong pass)', (done) => {
+		chai.request(endPoint)
+			.post('/users/passLogin')
+			.send({"email":"test1@email.com","pass":"testpass2"})
+			.end((err, res) => {
+				res.body.should.have.status(141);
+				done();
+			});
+	});
+
+	it('it shouldnt use password log in (user not found)', (done) => {
+		chai.request(endPoint)
+			.post('/users/passLogin')
+			.send({"email":"test2@email.com","pass":"testpass1"})
+			.end((err, res) => {
+				res.body.should.have.status(142);
+				done();
+			});
+	});
+
+	//token login
+	it('it should use token log in goodly(correct info)', (done) => {
+		chai.request(endPoint)
+			.post('/users/tokenLogin')
+			.send({"token":loginToken})
+			.end((err, res) => {
+				res.body.should.have.status(1);
+				done();
+			});
+	});
+
+	it('it shouldnt use token log in (wrong token)', (done) => {
+		chai.request(endPoint)
+			.post('/users/tokenLogin')
+			.send({"token":"fakeTokenHere"})
+			.end((err, res) => {
+				res.body.should.have.status(111);
+				done();
+			});
+	});
+
+	//change pass
+	it('it should change password (correct info)', (done) => {
+		chai.request(endPoint)
+			.put('/users/changePass')
+			.send({"token":token,})
+			.end((err, res) => {
+				res.body.should.have.status(111);
+				done();
+			});
+	});
+
+
 });
